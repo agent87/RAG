b8a785689abacee0347d2ead2e4a144b0d64de9c
Floris Nzabakira <nzafloris@gmail.com>
2024-06-12 11:58:02 +0200

    changing embeddings to openai

--- api/chatbot/utils.py
+++ api/chatbot/utils.py
@@ -1,6 +1,22 @@
-from langchain_community.document_loaders import DirectoryLoader, UnstructuredFileLoader
-from langchain_text_splitters import CharacterTextSplitter
+# from langchain_text_splitters import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import DirectoryLoader, UnstructuredFileLoader
+from langchain_openai import ChatOpenAI
+
+import os
+import shutil
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
 
 def initialize_db(
         embeddings,
@@ -18,16 +34,39 @@ def initialize_db(
 
     documents = loader.load()
 
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True)
     split_documents = text_splitter.split_documents(documents)
     
+    if(os.path.exists(db_name)):
+        shutil.rmtree(db_name)
+
     db = Chroma.from_documents(split_documents, embeddings, persist_directory=db_name)
     return db
 
-def search(query, db):
-    results = db.similarity_search_with_score(query)
+def search(query, model, db, threshold=0.7):
+    results = db.similarity_search_with_relevance_score(query)
+
+    if len(results) == 0 or results[0][1] < threshold:
+        return {
+            "code": 404,
+            "message": "Failed to retrieve relevant information.",
+            "query": query,
+            "score": results[0][1]       
+        }
+    
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query)
+
+    generated_text = model.predict(prompt)
+    
     result = {
         "query": query,
+        "generate text": generated_text,
         "content": results[0][0].page_content,
         "source": results[0][0].metadata["source"],
         "metadata": results[0][0].metadata,
--- main.py
+++ main.py
@@ -3,23 +3,31 @@ from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+import openai
+import os
 
 from api.chatbot.utils import initialize_db, search
 
 @hydra.main(config_path="conf", config_name="base", version_base=None)
 @hydra.main(config_path="conf", config_name="openai", version_base=None)
 def main(cfg: DictConfig):
+    # openai.api_key = cfg.openai.api_key
+    os.environ["OPENAI_API_KEY"] = cfg.openai.api_key
+    
     initialize_db(
-        HuggingFaceEmbeddings(),
+        OpenAIEmbeddings(),
         data_directory=cfg.data_directory,
         glob=cfg.glob,
         db_name=cfg.db_name
     )
 
+    model = ChatOpenAI()
+
     db = Chroma(persist_directory=cfg.db_name, embedding_function=HuggingFaceEmbeddings())
 
-    print(search("In the fourth quarter of 2023, GDP at current market prices was estimated at Frw 4,500 billion", db))
+    query = "In the fourth quarter of 2023, GDP at current market prices was estimated at Frw 4,500 billion"
+    print(search(query, model, db))
 
 if __name__ == "__main__":
     main()
